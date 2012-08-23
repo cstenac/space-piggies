880aaa70f0a042cc0ca8b15bd972697870ba9f63
Clément Stenac <clement.stenac@exalead.com>
2012-08-23 17:53:58 +0200

    Tests for LineStringFromMultiPoint

---
+++ src/test/java/org/apache/pig/piggybank/test/evaluation/spatial/TestLinestringFromMultiPoint.java
@@ -0,0 +1,45 @@
+package org.apache.pig.piggybank.test.evaluation.spatial;
+
+import static org.junit.Assert.*;
+
+import org.apache.pig.backend.executionengine.ExecException;
+import org.apache.pig.data.BagFactory;
+import org.apache.pig.data.DataBag;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.TupleFactory;
+import org.apache.pig.piggybank.evaluation.spatial.LineStringFromMultipoint;
+import org.junit.Test;
+
+public class TestLinestringFromMultiPoint {
+    private Tuple newTuple(String... points) throws ExecException {
+        Tuple t1 = TupleFactory.getInstance().newTuple(1);
+        DataBag bag = BagFactory.getInstance().newDefaultBag();
+        
+        for (String point : points) {
+            bag.add(TupleFactory.getInstance().newTuple(point));
+        }
+        t1.set(0, bag);
+        return t1;
+    }
+    
+    @Test
+    public void emptyBag() throws Exception {
+        Tuple t1 = TupleFactory.getInstance().newTuple(1);
+        t1.set(0, BagFactory.getInstance().newDefaultBag());
+        
+        LineStringFromMultipoint func = new LineStringFromMultipoint();
+        assertEquals("LINESTRING EMPTY", func.exec(t1));
+    }
+    @Test
+    public void regular1() throws Exception {
+        Tuple t1 = newTuple("POINT(0 0)", "POINT(1 1)");
+        LineStringFromMultipoint func = new LineStringFromMultipoint();
+        assertEquals("LINESTRING (0 0, 1 1)", func.exec(t1));
+    }
+    @Test
+    public void closed() throws Exception {
+        Tuple t1 = newTuple("POINT(0 0)", "POINT(1 1)", "POINT(0 0)");
+        LineStringFromMultipoint func = new LineStringFromMultipoint();
+        assertEquals("LINESTRING (0 0, 1 1, 0 0)", func.exec(t1));
+    }
+}
